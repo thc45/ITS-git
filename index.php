99f915e3063eb4fe8a4df4e52f9ff29a77d0ef4f
K <karoljoe@op.pl>
2017-03-29 16:08:16 +0200

    Added index.html
    Started working on project.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Portfolio">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<meta name="viewport" content="width=device-width; initial-scale=1.0;">
+
+	<title>Portfolio</title>
+	</head>
+		<body>
+			<div class="container">
+				<div class="header">
+					
+						<div class="topnav" id="myTopnav">
+						<div class="logo"><img src="img/logo.png" height="70" width="90"></div>
+							<ul>
+								<li>Home</li>
+								<li>Over mij</li>
+								<li>Contact</li>
+							</ul>
+						</div>
+				</div>
+			</div>
+		</body>
+	</html>
